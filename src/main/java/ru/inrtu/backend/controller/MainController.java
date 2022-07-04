40be21c2af6c5ff507f79cb68fedc9c29b1677e9
IStanovikhin <istanovikhin@croc.ru>
2022-07-04 12:51:33 +0300

    init

---
+++ src/main/java/ru/inrtu/backend/controller/MainController.java
@@ -0,0 +1,20 @@
+package ru.inrtu.backend.controller;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpResponse;
+
+@RestController
+@RequestMapping("/school-page/api")
+public class MainController {
+
+    @GetMapping("/testMethod")
+    public ResponseEntity<?> testController(){
+        System.out.println("Hello from main controller");
+        return ResponseEntity.ok("Hello from school-page app");
+    }
+}
